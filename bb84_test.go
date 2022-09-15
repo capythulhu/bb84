c4b635f42da60c6e948cf910a8c79237a7a12b1d
thzoid <thiagozoid@gmail.com>
2022-09-15 20:24:14 -0300

    add helper function

--- bb84_test.go
+++ bb84_test.go
@@ -1,8 +1,9 @@
 package bb84
 
 import (
-	"crypto/rand"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/tj/assert"
 )
@@ -10,46 +11,45 @@ import (
 // Key length in bytes
 const KEY_LENGTH = 8
 
-func TestSafeKeyDistribution(t *testing.T) {
-	aliceKey := make([]byte, KEY_LENGTH)
-	rand.Read(aliceKey)
+// Helper function for generating random bytes
+func randomBytes() []byte {
+	bytes := make([]byte, KEY_LENGTH)
+	rand.Read(bytes)
+	return bytes
+}
 
-	aliceFilters := make([]byte, KEY_LENGTH)
-	rand.Read(aliceFilters)
+// Alice send bits to Bob without any eavesdropping
+func TestSafeKeyDistribution(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+
+	aliceKey := randomBytes()
+	aliceFilters := randomBytes()
 
 	photons := BitsToPhotons(aliceKey, aliceFilters)
 
-	bobFilters := make([]byte, KEY_LENGTH)
-	rand.Read(bobFilters)
-
+	bobFilters := randomBytes()
 	bobKey := PhotonsToBits(photons, bobFilters)
 
 	aliceFinalKey, bobFinalKey := GenerateKeys(aliceKey, bobKey, aliceFilters, bobFilters)
-
 	assert.Equal(t, aliceFinalKey, bobFinalKey)
 }
 
+// Alice send bits to Bob with eavesdropping
 func TestUnsafeKeyDistribution(t *testing.T) {
-	aliceKey := make([]byte, KEY_LENGTH)
-	rand.Read(aliceKey)
+	rand.Seed(time.Now().Unix())
 
-	aliceFilters := make([]byte, KEY_LENGTH)
-	rand.Read(aliceFilters)
+	aliceKey := randomBytes()
+	aliceFilters := randomBytes()
 
 	photons := BitsToPhotons(aliceKey, aliceFilters)
 
-	eveFilters := make([]byte, KEY_LENGTH)
-	rand.Read(eveFilters)
-
 	// Eavesdrop on the photons
+	eveFilters := randomBytes()
 	PhotonsToBits(photons, eveFilters)
 
-	bobFilters := make([]byte, KEY_LENGTH)
-	rand.Read(bobFilters)
-
+	bobFilters := randomBytes()
 	bobKey := PhotonsToBits(photons, bobFilters)
 
 	aliceFinalKey, bobFinalKey := GenerateKeys(aliceKey, bobKey, aliceFilters, bobFilters)
-
 	assert.NotEqual(t, aliceFinalKey, bobFinalKey)
 }
